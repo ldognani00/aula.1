44684fffad8cec255b45f568f7ed20f2bb93973c
ldognani00 <ldognani68@gmail.com>
2026-09-18 01:04:41 +0000

    atvvdd

---
+++ nome-projeto/src/app/lista2/ex5/ex5.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ex5',
+  standalone: false,
+  templateUrl: './ex5.html',
+  styleUrl: './ex5.css',
+})
+export class Ex5 {
+  nomes: string[] = ['Ana', 'Bruno','Carlos', 'Daniela', 'Eduardo'];
+}
--- nome-projeto/src/app/lista2/lista2-module.ts
+++ nome-projeto/src/app/lista2/lista2-module.ts
@@ -6,9 +6,10 @@ import { Ex1 } from './ex1/ex1';
 import { Ex2 } from './ex2/ex2';
 import { Ex3 } from './ex3/ex3';
 import { Ex4 } from './ex4/ex4';
+import { Ex5 } from './ex5/ex5';
 
 @NgModule({
-  declarations: [Ex1, Ex2, Ex3, Ex4],
+  declarations: [Ex1, Ex2, Ex3, Ex4, Ex5],
   imports: [CommonModule, Lista2RoutingModule],
 })
 export class Lista2Module {}
